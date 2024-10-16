8dacc644e462fdff043076f457f0bd070868657b
PicoloSan <gustavo.sch.lucas@gmail.com>
2024-10-16 14:05:16 -0300

    Versao 0.001 do Projeto em Lazarus

---
+++ robo.py
@@ -0,0 +1,14 @@
+from tkinter import messagebox
+
+"""
+ret = messagebox.askokcancel("titulo", "mensagem")
+ret = messagebox.askquestion("titulo", "mensagem")
+ret = messagebox.askretrycancel("titulo", "mensagem")
+ret = messagebox.askyesno("titulo", "mensagem")
+ret = messagebox.askyesnocancel("titulo", "mensagem")
+ret = messagebox.showerror("titulo", "mensagem")
+ret = messagebox.showinfo("titulo", "mensagem")
+ret = messagebox.showwarning("titulo", "mensagem")
+"""
+
+ret = messagebox.showinfo("Robô", "Funcionou!")
