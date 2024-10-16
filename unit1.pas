ebef85db0e112bddc2655e81c70b53bc7f51e6a1
PicoloSan <gustavo.sch.lucas@gmail.com>
2024-10-16 14:49:22 -0300

    Versao 0.002 do Projeto em Lazarus

--- unit1.pas
+++ unit1.pas
@@ -5,7 +5,8 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, Buttons, ShellApi;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, Buttons,
+  StdCtrls, ShellApi, fpjson, jsonparser;
   // LCLIntf, FileUtil, StdCtrls;
 
 type
@@ -15,19 +16,46 @@ type
   TForm1 = class(TForm)
     BitBtn1: TBitBtn;
     BitBtn2: TBitBtn;
+    BitBtn3: TBitBtn;
+    BitBtn4: TBitBtn;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Edit5: TEdit;
+    Edit6: TEdit;
+    Edit7: TEdit;
+    Edit8: TEdit;
+    Edit9: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    OpenDialog1: TOpenDialog;
     TrayIcon1: TTrayIcon;
     procedure BitBtn1Click(Sender: TObject);
     procedure BitBtn2Click(Sender: TObject);
+    procedure BitBtn3Click(Sender: TObject);
+    procedure BitBtn4Click(Sender: TObject);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCreate(Sender: TObject);
     procedure TrayIcon1DblClick(Sender: TObject);
   private
-
+    function LerArquivoTexto(NomeArquivo: String): String;
+    procedure EscreverArquivoTexto(NomeArquivo, Conteudo: String);
   public
 
   end;
 
 var
   Form1: TForm1;
+  JSONString: String;
+  JsonObject: TJSONObject;
 
 implementation
 
@@ -58,10 +86,64 @@ begin
   ShellExecute(0, 'open', PChar(ProgramPath), '', '', 1);
 end;
 
+procedure TForm1.BitBtn3Click(Sender: TObject);
+var
+  CaminhoArquivo: string;
+begin
+  OpenDialog1 := TOpenDialog.Create(nil);
+  try
+    if OpenDialog1.Execute then
+    begin
+      CaminhoArquivo := OpenDialog1.FileName;
+      Edit3.Text := StringReplace(CaminhoArquivo, '\', '/', [rfReplaceAll]);
+    end;
+  finally
+    OpenDialog1.Free;
+  end;
+end;
+
+procedure TForm1.BitBtn4Click(Sender: TObject);
+begin
+  // Converter os dados para JSON
+  JsonObject := TJSONObject.Create;
+  JsonObject.Add('usr', Edit1.Text);
+  JsonObject.Add('pwd', Edit2.Text);
+  JsonObject.Add('file', Edit3.Text);
+  JsonObject.Add('sheet', Edit4.Text);
+  JsonObject.Add('column', Edit5.Text);
+  JsonObject.Add('line', Edit6.Text);
+  JsonObject.Add('URLlogin', Edit7.Text);
+  JsonObject.Add('URLrota', Edit8.Text);
+  JsonObject.Add('log', Edit9.Text);
+  JSONString := JsonObject.FormatJSON();
+  EscreverArquivoTexto('rotas.json', JSONString);
+  MessageDlg('Informação', 'Configuração salva com sucesso!', mtInformation, [mbOK], 0);
+end;
+
 procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
 begin
   // Garante que o ícone seja removido ao fechar o aplicativo
   TrayIcon1.Visible := False;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  // Supondo que JSONString contenha o JSON com as cinco strings
+  JSONString := LerArquivoTexto('rotas.json');
+
+  // Analisando o JSON
+  JsonObject := GetJSON(JSONString) as TJSONObject;
+
+  // Atribuindo os valores do JSON ao registro
+  Edit1.Text := JsonObject.Get('usr', '');
+  Edit2.Text := JsonObject.Get('pwd', '');
+  Edit3.Text := JsonObject.Get('file', '');
+  Edit4.Text := JsonObject.Get('sheet', '');
+  Edit5.Text := JsonObject.Get('column', '');
+  Edit6.Text := JsonObject.Get('line', '');
+  Edit7.Text := JsonObject.Get('URLlogin', '');
+  Edit8.Text := JsonObject.Get('URLrota', '');
+  Edit9.Text := JsonObject.Get('log', '');
 end;
 
 procedure TForm1.TrayIcon1DblClick(Sender: TObject);
@@ -70,6 +152,55 @@ begin
   TrayIcon1.Visible := False;
   // Application.Restore;
   Self.Show; // Exibe o formulário na barra de tarefas
+end;
+
+function TForm1.LerArquivoTexto(NomeArquivo: String): String;
+var
+  Arquivo: TextFile;
+  Linha: String;
+begin
+  Result := '';
+  try
+    AssignFile(Arquivo, NomeArquivo);
+    {$I-}
+    Reset(Arquivo);
+    {$I+}
+
+    if IOResult <> 0 then
+    begin
+      ShowMessage('Erro ao abrir o arquivo');
+      Exit;
+    end;
+
+    while not Eof(Arquivo) do
+    begin
+      ReadLn(Arquivo, Linha);
+      Result := Result + Linha + sLineBreak;
+    end;
+
+    CloseFile(Arquivo);
+  except
+    on E: Exception do
+    begin
+      Result := '';
+      ShowMessage('Erro ao ler o arquivo: ' + E.Message);
+    end;
+  end;
+end;
+
+procedure TForm1.EscreverArquivoTexto(NomeArquivo, Conteudo: String);
+var
+  Arquivo: TextFile;
+begin
+  try
+    AssignFile(Arquivo, NomeArquivo);
+    Rewrite(Arquivo);
+    Write(Arquivo, Conteudo);
+    CloseFile(Arquivo);
+  except
+    on E: Exception do
+      ShowMessage('Erro ao escrever no arquivo: ' + E.Message);
+  end;
 end;
 
 end.
